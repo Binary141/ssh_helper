d4a4d90e28a927c7926e150745b3f075c01ab612
Brendan Davidson <binary141@gmail.com>
2022-01-28 08:48:35 -0700

    Added more options to run script

--- ssh_helper.sh
+++ ssh_helper.sh
@@ -1,23 +1,48 @@
 term="gnome-terminal -- "
-#term="konsole -e " For KDE desktop environments mostly
-#term="xterm -e " Older terminal emulator
+#term="konsole -e " #For KDE desktop environments mostly
+#term="xterm -e " #Older terminal emulator
 
+#echo "Arg 1: $1"
+#echo "Arg 2: $2"
 
 username=""
 
-echo "What is the server and port you would like to connect to? "
-read server
+if [ ${#1} -gt 0 ]
+then
+	if [[ $1 == "-v" ]]
+	then
+		if [[ ${#2} -gt 0 ]]
+		then
+			server=$2
+			port=${server:~3}
 
-port=${server:~3}
+			echo "ssh $username@ssh.cs.dixie.edu -L $port:$server"
 
-echo "ssh $username@ssh.cs.dixie.edu -L $port:$server"
+			$term ssh $username@ssh.cs.dixie.edu -L $port:$server && sleep 3 && vncviewer localhost:$port
+		else
+			echo "Please provide a valid port"
+		fi
+	else
+		echo "Sorry, $1 is not a valid command"
+		echo "Please use -v to connect through VNC"
+	fi
 
-read -r -p "Do you want to connect to vnc? [y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY]) 
-	$term ssh $username@ssh.cs.dixie.edu -L $port:$server && sleep 5 && vncviewer localhost:$port
-        ;;
-    *)
-	ssh $username@ssh.cs.dixie.edu -L $port:$server
-        ;;
-esac
+else
+
+	echo "What is the server and port you would like to connect to? "
+	read server
+
+	port=${server:~3}
+
+	echo "ssh $username@ssh.cs.dixie.edu -L $port:$server"
+
+	read -r -p "Do you want to connect to vnc? [y/N] " response
+	case "$response" in
+	    [yY][eE][sS]|[yY]) 
+		$term ssh $username@ssh.cs.dixie.edu -L $port:$server && sleep 3 && vncviewer localhost:$port
+		;;
+	    *)
+		ssh $username@ssh.cs.dixie.edu -L $port:$server
+		;;
+	esac
+fi
